0daea57f51b13f01a9950f55760fe2868fda5aaa
noeljortiz <noelo12130613@gmail.com>
2023-01-24 09:42:38 -0600

    intellij com

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main{
     public static void main(String[]args){
         JFrame myFrame = new JFrame();
 //system.out.println(get)
+        //intellij comment
         BallP myBallp = new BallP();
         //myFrame.add(myDrawp);
         myFrame.setVisible(true);
